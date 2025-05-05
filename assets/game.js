d57be9ab360e792fc249c4dd4a4366e4855fdd82
leosupra <leosupra@protonmail.com>
2025-05-05 23:46:57 +0100

    Update game.js

--- assets/game.js
+++ assets/game.js
@@ -5,7 +5,7 @@ let dogX, dogY;
 let speedIncreaseTimer;
 let canvas;
 let lanes = [];
-let lanePercents = [ 0.10, 0.23, 0.36, 0.49, 0.62, 0.75 ];
+let lanePercents = [ 0.15, 0.23, 0.36, 0.49, 0.62, 0.75 ];
 
 function preload() {
   dog = loadImage('assets/rez.png');
