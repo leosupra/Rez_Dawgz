0fd6ca74e79ff4b8557646115f1246f8ab36c0f5
leosupra <leosupra@protonmail.com>
2025-05-05 22:28:53 +0100

    Update game.js

--- assets/game.js
+++ assets/game.js
@@ -34,7 +34,7 @@ function setup() {
 
   speedIncreaseTimer = millis();
 
-  const carHeight = height * 0.2;
+  const carHeight = height * 0.15;
   const carWidth = carHeight * (370 / 800);
   const lanesCount = 6;
   lanes = [];
@@ -77,7 +77,7 @@ function setup() {
 function draw() {
   background(bg);
 
-  const carHeight = height * 0.2;
+  const carHeight = height * 0.15;
   const carWidth = carHeight * (370 / 800);
 
   for (let lane of lanes) {
